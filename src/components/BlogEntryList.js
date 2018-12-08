00973a8fc3883ea42d1c0934d0d99c1af180cc69
Randyjp <randyjp125@gmail.com>
2018-12-08 12:19:53 -0400

    Linking blog entries and adding prop-types

--- src/components/BlogEntryList.js
+++ src/components/BlogEntryList.js
@@ -1,28 +1,45 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import Card from 'react-bulma-components/lib/components/card';
 import Content from 'react-bulma-components/lib/components/content';
+import PropTypes from 'prop-types';
 
 const BlongEntryList = ({ blog }) => {
   const {
     timeToRead,
     fields: { slug },
-    frontmatter: { title, author, date },
+    frontmatter: { title, date },
   } = blog;
 
   return (
-    <Card>
-      <Card.Image
-        size={128}
-        src="https://assets.goodstatic.com/s3/magazine/others/meta/GoodLogo2.png"
-      />
-      <Card.Content>
-        <Content>
-          {title} ~ <span>{timeToRead} minutes read</span> <br />
-          <time dateTime={date}>{date}</time>
-        </Content>
-      </Card.Content>
-    </Card>
+    <Link to={slug}>
+      <Card>
+        <Card.Image
+          size={128}
+          src="https://assets.goodstatic.com/s3/magazine/others/meta/GoodLogo2.png"
+        />
+        <Card.Content>
+          <Content>
+            {title} ~ <span>{timeToRead} minutes read</span> <br />
+            <time dateTime={date}>{date}</time>
+          </Content>
+        </Card.Content>
+      </Card>
+    </Link>
   );
 };
 
+BlongEntryList.propTypes = {
+  blog: PropTypes.shape({
+    timeToRead: PropTypes.number.isRequired,
+    frontmatter: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      author: PropTypes.array.isRequired,
+      date: PropTypes.string.isRequired,
+    }).isRequired,
+    fields: PropTypes.shape({
+      slug: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 export default BlongEntryList;

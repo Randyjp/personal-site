c0f8bc3c637c1ccf0f0e1400b97e5ec8d55681d5
Randyjp <randyjp125@gmail.com>
2018-12-16 13:40:17 -0400

    nav-bar with and acive state

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'gatsby';
 import Navbar from 'react-bulma-components/lib/components/navbar';
+import Box from 'react-bulma-components/lib/components/box';
+import Container from 'react-bulma-components/lib/components/container';
 
 class Nav extends Component {
   state = {
@@ -10,28 +12,52 @@ class Nav extends Component {
   render() {
     const { open } = this.state;
     return (
-      <Navbar color="white" active={open}>
-        <Navbar.Brand>
-          <Navbar.Burger onClick={() => this.setState({ open: !open })} />
-        </Navbar.Brand>
-        <Navbar.Menu>
-          <Navbar.Container position="end">
-            {/* adds classname navbar-item to use bulma styles on gatsby links */}
-            <Link className="navbar-item" to="#">
-              About
-            </Link>
-            <Link className="navbar-item" to="#">
-              Services
-            </Link>
-            <Link className="navbar-item" to="/blog-list">
-              Blog
-            </Link>
-            <Link className="navbar-item" to="#">
-              Resume
-            </Link>
-          </Navbar.Container>
-        </Navbar.Menu>
-      </Navbar>
+      <Box>
+        <Navbar color="white" active={open}>
+          <Container>
+            <Navbar.Brand>
+              <Navbar.Item renderAs="a" href="#">
+                <img
+                  src="https://bulma.io/images/bulma-logo.png"
+                  alt="Bulma: a modern CSS framework based on Flexbox"
+                  width="112"
+                  height="28"
+                />
+              </Navbar.Item>
+              <Navbar.Burger onClick={() => this.setState({ open: !open })} />
+            </Navbar.Brand>
+            <Navbar.Menu>
+              <Navbar.Container position="end">
+                {/* adds classname navbar-item to use bulma styles on gatsby links */}
+                <Link
+                  className="navbar-item"
+                  activeClassName="is-active"
+                  to="#"
+                >
+                  About
+                </Link>
+                <Link className="navbar-item" to="#">
+                  Services
+                </Link>
+                <Link
+                  className="navbar-item"
+                  activeClassName="is-active"
+                  to="/blog-list"
+                >
+                  Blog
+                </Link>
+                <Link
+                  className="navbar-item"
+                  activeClassName="is-active"
+                  to="#"
+                >
+                  Resume
+                </Link>
+              </Navbar.Container>
+            </Navbar.Menu>
+          </Container>
+        </Navbar>
+      </Box>
     );
   }
 }
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,20 @@
 import Typography from 'typography';
 import parnassusTheme from 'typography-theme-parnassus';
 
+parnassusTheme.overrideStyles = () => ({
+  a: {
+    color: '#375c85',
+    textDecoration: 'none',
+    boxShadow: 'none',
+  },
+});
+
 const typography = new Typography(parnassusTheme);
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== 'production') {
+  typography.injectStyles();
+}
 
 export default typography;
 export const { rhythm } = typography;

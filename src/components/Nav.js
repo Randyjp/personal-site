4315ee480c573069e5163f4d3ef867d4ceee52bb
Randyjp <randyjp125@gmail.com>
2018-12-17 20:11:11 -0400

    adding icons and styling nav-bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import Navbar from 'react-bulma-components/lib/components/navbar';
 import Box from 'react-bulma-components/lib/components/box';
 import Container from 'react-bulma-components/lib/components/container';
+import Icon from 'react-bulma-components/lib/components/icon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBlog,
+  faFilePdf,
+  faKeyboard,
+  faAddressCard,
+} from '@fortawesome/free-solid-svg-icons';
 
 class Nav extends Component {
   state = {
@@ -12,9 +21,13 @@ class Nav extends Component {
   render() {
     const { open } = this.state;
     return (
-      <Box>
-        <Navbar color="white" active={open}>
-          <Container>
+      <Container
+        style={{
+          marginTop: '1.5rem'
+        }}
+      >
+        <Box>
+          <Navbar color="white" active={open}>
             <Navbar.Brand>
               <Navbar.Item renderAs="a" href="#">
                 <img
@@ -29,37 +42,42 @@ class Nav extends Component {
             <Navbar.Menu>
               <Navbar.Container position="end">
                 {/* adds classname navbar-item to use bulma styles on gatsby links */}
-                <Link
-                  className="navbar-item"
-                  activeClassName="is-active"
-                  to="#"
-                >
-                  About
-                </Link>
-                <Link className="navbar-item" to="#">
-                  Services
-                </Link>
-                <Link
-                  className="navbar-item"
-                  activeClassName="is-active"
-                  to="/blog-list"
-                >
-                  Blog
-                </Link>
-                <Link
-                  className="navbar-item"
-                  activeClassName="is-active"
-                  to="#"
-                >
-                  Resume
-                </Link>
+                <NavItemLink name="About" icon={faAddressCard} to="#" />
+                <NavItemLink name="Services" icon={faKeyboard} to="#" />
+                <NavItemLink name="Blog" icon={faBlog} to="/blog-list" />
+                <NavItemLink name="Resume" icon={faFilePdf} to="#" />
               </Navbar.Container>
             </Navbar.Menu>
-          </Container>
-        </Navbar>
-      </Box>
+          </Navbar>
+        </Box>
+      </Container>
     );
   }
 }
+
+const NavItemLink = ({ name, icon, to }) => (
+  <Link className="navbar-item" activeClassName="is-active" to={to}>
+    <span
+      style={{
+        marginRight: '0.25rem',
+      }}
+    >
+      <Icon color="info">
+        <FontAwesomeIcon icon={icon} />
+      </Icon>
+    </span>
+    <span>{name}</span>
+  </Link>
+);
+
+NavItemLink.propTypes = {
+  name: PropTypes.string.isRequired,
+  icon: PropTypes.shape({
+    icon: PropTypes.array.isRequired,
+    iconName: PropTypes.string.isRequired,
+    prefix: PropTypes.string.isRequired,
+  }).isRequired,
+  to: PropTypes.string.isRequired,
+};
 
 export default Nav;

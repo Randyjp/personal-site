1d04334d66453a87d257be51e8da4c5a359a5e75
Randyjp <randyjp125@gmail.com>
2018-12-18 20:07:21 -0400

    adds pagination to blog list

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,20 @@
-const path = require("path");
-const { createFilePath } = require("gatsby-source-filesystem");
+const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
+const createPaginatedPages = require('gatsby-paginate');
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
-  if (node.internal.type === "MarkdownRemark") {
+  if (node.internal.type === 'MarkdownRemark') {
     // creates the slugs based on the file name
     const slug = createFilePath({
       node,
       getNode,
-      trailingSlash: false
+      trailingSlash: false,
     });
     createNodeField({
       node,
-      name: "slug",
-      value: slug
+      name: 'slug',
+      value: slug,
     });
   }
 };
@@ -22,9 +23,9 @@ exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       alias: {
-        "./_variables.sass": path.resolve(__dirname, "src/_variables.sass")
-      }
-    }
+        './_variables.sass': path.resolve(__dirname, 'src/_variables.sass')
+      },
+    },
   });
 };
 
@@ -34,18 +35,21 @@ exports.createPages = ({ graphql, actions }) => {
   return new Promise((resolve, reject) => {
     graphql(`
       {
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
           edges {
             node {
               frontmatter {
                 title
+                author
+                date
+                attachments {
+                  publicURL
+                }
               }
               fields {
                 slug
               }
+              timeToRead
             }
           }
         }
@@ -62,14 +66,21 @@ exports.createPages = ({ graphql, actions }) => {
           blogPosts.length - (index + 1) > 0 ? blogPosts[index + 1].node : null;
         createPage({
           path: node.fields.slug,
-          component: path.resolve("./src/templates/blog-post.js"),
+          component: path.resolve('./src/templates/blog-post.js'),
           context: {
             // data passed here will be availble for the template for page queries
             slug: node.fields.slug,
             previous,
-            next
-          }
+            next,
+          },
         });
+      });
+      createPaginatedPages({
+        edges: blogPosts,
+        createPage,
+        pageTemplate: './src/templates/blog-list.js',
+        pageLength: 8,
+        pathPrefix: 'blog-list',
       });
       resolve();
     });
---
+++ src/templates/blog-list.js
@@ -0,0 +1,103 @@
+import React, { Fragment } from 'react';
+import { Link } from 'gatsby';
+import Columns from 'react-bulma-components/lib/components/columns';
+// eslint-disable-next-line
+import Pagination from "react-bulma-components/lib/components/pagination";
+import PropTypes from 'prop-types';
+import BlongEntryList from '../components/BlogEntryList';
+import BasicLayout from '../components/BasicLayout';
+
+const BlogList = ({ pageContext }) => {
+  console.log(pageContext);
+  const { group, index, pageCount } = pageContext;
+  const next = index < pageCount ? index + 1 : null;
+  const previous = index > 1 ? index - 1 : null;
+  return (
+    <BasicLayout
+      render={() => {
+        const blogs = group.map(blog => (
+          <Columns.Column size="one-quarter" key={blog.node.fields.slug}>
+            <BlongEntryList blog={blog.node} />
+          </Columns.Column>
+        ));
+
+        return (
+          <Fragment>
+            {blogs}
+            <BlogPagination previous={previous} next={next} />
+          </Fragment>
+        );
+      }}
+    />
+  );
+};
+
+const BlogPagination = ({ previous, next }) => {
+  const localPrevious = previous === 1 ? '' : previous;
+  return (
+    <Columns.Column
+      size="full"
+      style={{
+        marginTop: '60px',
+      }}
+    >
+      <nav
+        className="pagination is-centered is-medium"
+        role="navigation"
+        aria-label="pagination"
+      >
+        {previous && (
+          <Link
+            className="pagination-previous"
+            to={`/blog-list/${localPrevious}`}
+            disabled
+          >
+            ← Newer Posts
+          </Link>
+        )}
+        {next && (
+          <Link className="pagination-next" to={`/blog-list/${next}`}>
+            Older Posts →
+          </Link>
+        )}
+      </nav>
+    </Columns.Column>
+  );
+};
+
+BlogPagination.propTypes = {
+  previous: PropTypes.number,
+  next: PropTypes.number,
+};
+
+BlogPagination.defaultProps = {
+  previous: null,
+  next: null,
+};
+
+BlogList.propTypes = {
+  pageContext: PropTypes.shape({
+    pathPrefix: PropTypes.string.isRequired,
+    pageCount: PropTypes.number.isRequired,
+    group: PropTypes.arrayOf(
+      PropTypes.shape({
+        edges: PropTypes.shape({
+          timeToRead: PropTypes.number.isRequired,
+          node: PropTypes.shape({
+            frontmatter: PropTypes.shape({
+              title: PropTypes.string.isRequired,
+              author: PropTypes.string.isRequired,
+              date: PropTypes.string.isRequired,
+              attachments: PropTypes.arrayOf(PropTypes.object).isRequired,
+            }).isRequired,
+          }).isRequired,
+          fields: PropTypes.shape({
+            slug: PropTypes.string.isRequired,
+          }).isRequired,
+        }),
+      }).isRequired
+    ),
+  }).isRequired,
+};
+
+export default BlogList;

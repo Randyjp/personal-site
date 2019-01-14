a8cdc27dc7164d613fdab4b8dbfe269e7ae926f9
Randyjp <randyjp125@gmail.com>
2019-01-14 14:29:54 -0400

    Migrates contact form to new basic layout

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import Columns from 'react-bulma-components/lib/components/columns';
+import Container from 'react-bulma-components/lib/components/container';
+import Section from 'react-bulma-components/lib/components/section';
 import BasicLayout from '../components/BasicLayout';
 import ContactForm from '../components/ContactForm';
 
 export default () => (
   <BasicLayout
+    withNav
+    withFooter
     render={() => (
-      <Columns.Column size="three-fifths" offset="one-fifth">
-        <h1>Let&apos;s get in touch</h1>
-        <ContactForm />
-      </Columns.Column>
+      <Section>
+        <Container>
+          <Columns>
+            <Columns.Column size="three-fifths" offset="one-fifth">
+              <h1>Let&apos;s get in touch</h1>
+              <ContactForm />
+            </Columns.Column>
+          </Columns>
+        </Container>
+      </Section>
     )}
   />
 );

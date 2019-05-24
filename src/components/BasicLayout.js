b7be15cc58aeaf0f8bd8862723b459fc6d4657a0
Randy Perez <randyjp125@gmail.com>
2019-05-24 19:26:39 -0400

    Removes bulma from card grid (#15)

--- src/components/BasicLayout.js
+++ src/components/BasicLayout.js
@@ -8,40 +8,89 @@ import Nav from './Nav';
 import Footer from './Footer';
 
 const StyledContainer = styled.div`
-  max-width: 64rem;
   margin: auto;
   padding-left: 1rem;
   padding-right: 1rem;
 
   ${breakpoint('tablet')`
+    max-width: 64rem;
     padding-left: 3.5rem;
     padding-right: 3.5rem;
   `}
 `;
-// TODO: remove withContainer
 
-const BasicLayout = ({ children, withNav, withFooter, withContainer }) => (
-  <ThemeProvider theme={theme}>
-    <Fragment>
-      {withNav && <Nav />}
-      {withContainer ? <StyledContainer>{children}</StyledContainer> : children}
-      {withFooter && <Footer />}
-      <GlobalStyle />
-    </Fragment>
-  </ThemeProvider>
-);
+const StyledWideContainer = styled(StyledContainer)`
+  ${breakpoint('desktop')`
+    max-width: 84rem;
+  `}
+`;
+
+const CONTAINER_TYPE = Object.freeze({
+  WIDE: 'wide',
+  STANDARD: 'standard',
+  NOTHING: 'nothing',
+});
+
+const BasicLayout = ({ children, withNav, withFooter, containerType }) => {
+  function getContainer() {
+    switch (containerType) {
+      case CONTAINER_TYPE.WIDE:
+        return <StyledWideContainer>{children}</StyledWideContainer>;
+      case CONTAINER_TYPE.STANDARD:
+        return <StyledContainer>{children}</StyledContainer>;
+      default:
+        return children;
+    }
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        {withNav && <Nav />}
+        {getContainer()}
+        {withFooter && <Footer />}
+        <GlobalStyle />
+      </Fragment>
+    </ThemeProvider>
+  );
+};
 
 BasicLayout.propTypes = {
   children: PropTypes.element.isRequired,
   withNav: PropTypes.bool,
   withFooter: PropTypes.bool,
-  withContainer: PropTypes.bool,
+  containerType: PropTypes.string,
 };
 
 BasicLayout.defaultProps = {
   withNav: true,
   withFooter: true,
-  withContainer: true,
+  containerType: CONTAINER_TYPE.STANDARD,
+};
+
+const WideLayout = ({ children, withNav, withFooter, containerType }) => {
+  return (
+    <BasicLayout
+      withNav={withNav}
+      withFooter={withFooter}
+      containerType={containerType}
+    >
+      {children}
+    </BasicLayout>
+  );
+};
+
+WideLayout.propTypes = {
+  children: PropTypes.element.isRequired,
+  withNav: PropTypes.bool,
+  withFooter: PropTypes.bool,
+  containerType: PropTypes.string,
+};
+
+WideLayout.defaultProps = {
+  withNav: true,
+  withFooter: true,
+  containerType: CONTAINER_TYPE.WIDE,
 };
 
 export default BasicLayout;
+export { WideLayout, CONTAINER_TYPE };

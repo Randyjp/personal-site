02a0b42448a55103de6d58d5f48897df0c4c3393
Randyjp <randyjp125@gmail.com>
2021-02-27 18:05:06 -0400

    update modules to minor versions

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import BasicLayout from '../components/BasicLayout';
 import ContactForm from '../components/ContactForm';
 
-export default () => (
+const Contact = () => (
   <BasicLayout>
     <React.Fragment>
       <h1>Let&apos;s get in touch</h1>
@@ -10,3 +10,5 @@ export default () => (
     </React.Fragment>
   </BasicLayout>
 );
+
+export default Contact;
